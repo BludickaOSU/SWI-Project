b4b7f6728e4b90e1bec70f96ebe96a57fcd7ddcd
Matej cerveny <cerveny.mat@gmail.com>
2024-02-16 22:21:40 +0100

    springboot

---
+++ FirstBootstrapProject/src/main/java/cz/osu/FirstBootstrapProject/FirstBootstrapProjectApplication.java
@@ -0,0 +1,16 @@
+package cz.osu.FirstBootstrapProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBootstrapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBootstrapProjectApplication.class, args);
+	}
+
+
+
+
+}
---
+++ FirstBootstrapProject/src/test/java/cz/osu/FirstBootstrapProject/FirstBootstrapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.FirstBootstrapProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBootstrapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
