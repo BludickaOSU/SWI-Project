43b8e0f0924178545ce0bb54ced75635a715fa6d
Matej cerveny <cerveny.mat@gmail.com>
2024-02-17 13:40:04 +0100

    Začatek

---
+++ BE_SWI1/src/test/java/cz/osu/BeSwi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSwi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
