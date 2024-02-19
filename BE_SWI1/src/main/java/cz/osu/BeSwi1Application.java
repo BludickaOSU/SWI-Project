98580136c122c8a49d694d1bffa51cc847182674
Matej Cerveny <cerveny.mat@gmail.com>
2024-02-19 09:44:42 +0100

    Zkouska

---
+++ BE_SWI1/src/main/java/cz/osu/BeSwi1Application.java
@@ -0,0 +1,13 @@
+package cz.osu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSwi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSwi1Application.class, args);
+    }
+
+}
